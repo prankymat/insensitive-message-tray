66956551d9899608e7d28a021b7a0e6465bc5361
Matthew Wo <9029537@gmail.com>
2016-01-18 11:35:10 -0800

    quick and dirty fix for #8

--- extension.js
+++ extension.js
@@ -2,27 +2,36 @@
 const LayoutManager = imports.ui.main.layoutManager;
 const Shell = imports.gi.Shell;
 
+const Mainloop = imports.mainloop;
+
 const MessageTray = imports.ui.main.messageTray;
 let _myDwell = 0;
+
+var loop = true;
 
 function init() { }
 
 function enable() {
+  Mainloop.timeout_add(1000, function(){
     if("_trayPressure" in LayoutManager) {
         LayoutManager._trayPressure._keybindingMode =
             Shell.KeyBindingMode.OVERVIEW;
     }
-        
+
     _myDwell = MessageTray._trayDwellTimeout;
     MessageTray._trayDwellTimeout = function() { return false; };
+
+    return loop;
+  });
 }
 
 function disable() {
+  loop = false;
     if("_trayPressure" in LayoutManager) {
-        LayoutManager._trayPressure._keybindingMode = 
+        LayoutManager._trayPressure._keybindingMode =
             Shell.KeyBindingMode.NORMAL | Shell.KeyBindingMode.OVERVIEW;
     }
-    
+
     if(_myDwell) {
         MessageTray._trayDwellTimeout = _myDwell;
         _myDwell = 0;
